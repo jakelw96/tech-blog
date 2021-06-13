2dce35f6ff7b06d1f5dda27205b385d3029106b9
jakelw96 <jake.lw96@gmail.com>
2021-06-13 16:39:48 -0600

    Created html file to help create handlebars. Created files for handlebars and working on login routes

--- controllers/api/user-routes.js
+++ controllers/api/user-routes.js
@@ -61,6 +61,46 @@ router.post('/', (req, res) => {
     });
 });
 
+// Login
+router.post('/login', (req, res) => {
+    User.findOne({
+        where: {
+            username: req.body.username
+        }
+    })
+    .then(dbUserData => {
+        if (!dbUserData) {
+            res.status(400).json({ message: 'No username found' });
+            return;
+        }
+
+        const validPassword = dbUserData.checkPassword(req.body.password);
+
+        if (!validPassword) {
+            res.status(400).json({ message: 'Incorrect password!' });
+            return;
+        }
+
+        req.session.save(() => {
+            req.session.user_id = dbUserData.id;
+            req.session.username = dbUserData.username;
+            req.session.loggedIn = true;
+
+            res.json({ user: dbUserData, message: 'You are now logged in' });
+        });
+    });
+});
+
+router.post('/logout', (req, res) => {
+    if (req.session.loggedIn) {
+        req.session.destroy(() => {
+            res.status(204).end();
+        })
+    } else {
+        res.status(404).end();
+    }
+});
+
 // Updates the user
 router.put('/:id', (req, res) => {
     User.update(req.body, {
