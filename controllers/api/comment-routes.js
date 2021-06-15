bcfecf145a52ea8bb64dff088b83ed9ddd27df26
jakelw96 <jake.lw96@gmail.com>
2021-06-15 00:56:20 -0600

    Created comment form and logic

--- controllers/api/comment-routes.js
+++ controllers/api/comment-routes.js
@@ -15,16 +15,18 @@ router.get('/', (req, res) => {
 
 // Creates a comment
 router.post('/', (req, res) => {
-    Comment.create({
-        comment_text: req.body.comment_text,
-        post_id: req.body.post_id,
-        user_id: req.body.user_id
-    })
-    .then(dbCommentData => res.json(dbCommentData))
-    .catch(err => {
-        console.log(err);
-        res.status(500).json(err);
-    });
+    if (req.session) {
+        Comment.create({
+            comment_text: req.body.comment_text,
+            post_id: req.body.post_id,
+            user_id: req.session.user_id
+        })
+        .then(dbCommentData => res.json(dbCommentData))
+        .catch(err => {
+            console.log(err);
+            res.status(500).json(err);
+        });
+    };
 });
 
 // Delete a comment
